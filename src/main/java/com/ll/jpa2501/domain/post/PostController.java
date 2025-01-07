c483e8926664d0aa9fd82f76307e73214aa80049
sik2 <bickwang123@gmail.com>
2025-01-07 15:12:34 +0900

    transtion

--- src/main/java/com/ll/jpa2501/domain/post/PostController.java
+++ src/main/java/com/ll/jpa2501/domain/post/PostController.java
@@ -2,6 +2,7 @@ package com.ll.jpa2501.domain.post;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,7 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping("/{id}")
-    public Post showPost(Long id) {
+    public Post showPost(@PathVariable("id") Long id) {
         return postService.getPost(id);
     }
 }
